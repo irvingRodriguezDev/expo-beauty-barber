eff95f71ca7d2ef4195eec88939ac2085c34b41f
irving rodriguez <al221810739@gmail.com>
2026-07-15 09:23:17 -0600

    fix route for accepted link for fc post

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,6 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
   Outlet,
 } from "react-router-dom";
 import TicketView from "./components/Pages/TicketDetail";
@@ -21,28 +20,26 @@ function Home() {
   return (
     <Router>
       <Routes>
-        {/* Redirección inicial: de "/" a la base del evento */}
-        <Route path='/' element={<Navigate to='/' replace />} />
-
-        {/* Grupo de rutas con la misma base */}
+        {/* Grupo de rutas principal */}
         <Route path='/'>
-          {/* Esta es la ruta index (la raíz del grupo) */}
+          {/* URL: / (Página de inicio) */}
           <Route index element={<App />} />
-          <Route path='/evento/:slug' element={<EventDetai />} />
-          {/* Las sub-rutas se vuelven relativas a la base */}
-          {/* URL resultante:/mis-boletos */}
-          <Route path='mis-boletos' element={<MyTickets />} />4
-          {/* URL resultante: /payment-success */}
-          <Route path='/payment-success' element={<SuccessPage />} />
-          <Route path='/payment-error' element={<ErrorPage />} />
-          {/* URL resultante: /ticket/:code */}
+
+          {/* URL: /evento/nombre-del-evento (Ignora el ?fbclid= automáticamente) */}
+          <Route path='evento/:slug' element={<EventDetai />} />
+
+          {/* URLs de usuario */}
+          <Route path='mis-boletos' element={<MyTickets />} />
+          <Route path='payment-success' element={<SuccessPage />} />
+          <Route path='payment-error' element={<ErrorPage />} />
           <Route path='ticket/:code' element={<TicketView />} />
-          {/* URL resultante:/admin/check-in */}
+
+          {/* URLs de administración */}
           <Route path='admin/check-in' element={<ScannerView />} />
           <Route path='admin/create-event' element={<AdminCreateEvent />} />
         </Route>
 
-        {/* Ruta 404 general */}
+        {/* Ruta 404 general para cualquier URL no registrada */}
         <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
@@ -50,29 +47,3 @@ function Home() {
 }
 
 export default Home;
-
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// import TicketView from "./components/Pages/TicketDetail";
-// import ScannerView from "./components/Pages/ScannerView";
-// import SuccessPage from "./components/Pages/SuccessPage";
-// import MyTickets from "./components/Pages/MyTickets";
-// import ErrorPage from "./components/Pages/ErrorPage";
-// import NotFound from "./components/Pages/NotFound";
-// import ComingSoon from "./components/Pages/CommingSoon";
-
-// function Home() {
-//   return (
-//     <Router>
-//       <Routes>
-//         {/* Página principal */}
-//         <Route path='/' element={<ComingSoon />} />
-
-//         {/* 404 */}
-//         <Route path='*' element={<NotFound />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default Home;
